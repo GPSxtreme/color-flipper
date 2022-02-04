a7b8fd01bf419e11ac77eeeb6df53e82921124d9
Royal-lobster <srujangs8@gmail.com>
2022-02-04 21:44:24 +0530

    final commit

--- script.js
+++ script.js
@@ -1,169 +1,39 @@
-//color hex codes array
-const colors = [
-  "f0f8ff",
-  "faebd7",
-  "00ffff",
-  "7fffd4",
-  "f0ffff",
-  "f5f5dc",
-  "ffe4c4",
-  "000000",
-  "ffebcd",
-  "0000ff",
-  "8a2be2",
-  "a52a2a",
-  "deb887",
-  "5f9ea0",
-  "7fff00",
-  "d2691e",
-  "ff7f50",
-  "6495ed",
-  "fff8dc",
-  "dc143c",
-  "00ffff",
-  "00008b",
-  "008b8b",
-  "b8860b",
-  "a9a9a9",
-  "006400",
-  "bdb76b",
-  "8b008b",
-  "556b2f",
-  "ff8c00",
-  "9932cc",
-  "8b0000",
-  "e9967a",
-  "8fbc8f",
-  "483d8b",
-  "2f4f4f",
-  "00ced1",
-  "9400d3",
-  "ff1493",
-  "00bfff",
-  "696969",
-  "1e90ff",
-  "d19275",
-  "b22222",
-  "fffaf0",
-  "228b22",
-  "ff00ff",
-  "dcdcdc",
-  "f8f8ff",
-  "ffd700",
-  "daa520",
-  "808080",
-  "008000",
-  "adff2f",
-  "f0fff0",
-  "ff69b4",
-  "cd5c5c",
-  "4b0082",
-  "fffff0",
-  "f0e68c",
-  "e6e6fa",
-  "fff0f5",
-  "7cfc00",
-  "fffacd",
-  "add8e6",
-  "f08080",
-  "e0ffff",
-  "fafad2",
-  "d3d3d3",
-  "90ee90",
-  "ffb6c1",
-  "ffa07a",
-  "20b2aa",
-  "87cefa",
-  "8470ff",
-  "778899",
-  "b0c4de",
-  "ffffe0",
-  "00ff00",
-  "32cd32",
-  "faf0e6",
-  "ff00ff",
-  "800000",
-  "66cdaa",
-  "0000cd",
-  "ba55d3",
-  "9370d8",
-  "3cb371",
-  "7b68ee",
-  "00fa9a",
-  "48d1cc",
-  "c71585",
-  "191970",
-  "f5fffa",
-  "ffe4e1",
-  "ffe4b5",
-  "ffdead",
-  "000080",
-  "fdf5e6",
-  "808000",
-  "6b8e23",
-  "ffa500",
-  "ff4500",
-  "da70d6",
-  "eee8aa",
-  "98fb98",
-  "afeeee",
-  "d87093",
-  "ffefd5",
-  "ffdab9",
-  "cd853f",
-  "ffc0cb",
-  "dda0dd",
-  "b0e0e6",
-  "800080",
-  "ff0000",
-  "bc8f8f",
-  "4169e1",
-  "8b4513",
-  "fa8072",
-  "f4a460",
-  "2e8b57",
-  "fff5ee",
-  "a0522d",
-  "c0c0c0",
-  "87ceeb",
-  "6a5acd",
-  "708090",
-  "fffafa",
-  "00ff7f",
-  "4682b4",
-  "d2b48c",
-  "008080",
-  "d8bfd8",
-  "ff6347",
-  "40e0d0",
-  "ee82ee",
-  "d02090",
-  "f5deb3",
-  "ffffff",
-  "f5f5f5",
-  "ffff00",
-  "9acd32",
-];
-//random number generator
-const randomNumber = function () {
-  return Math.floor(Math.random() * colors.length);
+const button = document.getElementById("button");
+const colorCode = document.getElementById("colorName");
+const copyBtn = document.getElementById("button_copy");
+
+let color = "";
+let colorName = "";
+let colorHexCode = "";
+
+//change color
+let changeColor = async () => {
+  // generate random color
+  let r = () => Math.floor(Math.random() * 255);
+  color = `rgb(${r()}, ${r()}, ${r()})`;
+
+  //call the color api to get the color name and hex code
+  let res = await fetch(`https://www.thecolorapi.com/id?rgb=${color}`);
+  let body = await res.json();
+  console.log(body.hex.value, body.name.value);
+
+  // update colorName and colorHexCode
+  colorName = body.name.value;
+  colorHexCode = body.hex.value;
+
+  // update document with new color
+  document.body.style.background = color;
+  colorCode.textContent = `${colorHexCode} [${colorName}]`;
 };
-//naming button
-const button = document.querySelector("#button");
-//naming color display
-const colorName = document.getElementById("colorName");
+
+// call color change first time on refresh
+changeColor();
+
 //function to change color
-button.addEventListener("click", function () {
-  let no = randomNumber();
-  const bgColor = colors[no];
-  document.body.style.background = `#${bgColor}`;
-  colorName.textContent = `#${bgColor}`;
-  //naming copy button
-  const copy = document.querySelector("#button_copy");
-  //code
-  const code = `#${bgColor}`;
-  //when copy code button pressed copy color code to clipboard
-  copy.addEventListener("click", function () {
-    navigator.clipboard.writeText(code);
-  });
+button.addEventListener("click", changeColor);
+
+//when copy code button pressed copy color code to clipboard
+copyBtn.addEventListener("click", function () {
+  navigator.clipboard.writeText(colorHexCode);
+  alert(`Copied ${colorHexCode}`);
 });
